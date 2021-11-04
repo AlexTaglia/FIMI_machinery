30152f5977b72e6e69bdd07feacb3d28ed3bcea8
AlexTaglia <alex.tagliabue@gmail.com>
2021-11-04 17:11:23 +0100

    modifiche generali

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 // Change color to navbar on sroll
-  $(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-  })
+
+var navbar = document.querySelector('nav');
+window.onscroll = function() {
+
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled');
+    navbar.classList.remove('navbar-dark');
+    navbar.classList.add('navbar-light');
+
+  } else {
+    navbar.classList.remove('scrolled');
+    navbar.classList.remove('navbar-light');
+    navbar.classList.add('navbar-dark');
+  }
+}
